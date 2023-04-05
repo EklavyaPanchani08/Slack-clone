e909be857e7e40ee719964367a769438891e6916
Eklavya_Panchani <104212977+EklavyaPanchani08@users.noreply.github.com>
2023-04-05 22:30:41 +0530

    changes in app

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+          {/* left side */}
+      </div>
+      <div>
+          {/* center side */}
+          {/* clock icon */}
+          {/* search icon */}
+      </div>
+      <div>
+          {/* right side */}
+          {/* Help icon */}
+          {/* logedin user profile */}
+      </div>
+    </div>
+  )
+}
+
+export default Header
